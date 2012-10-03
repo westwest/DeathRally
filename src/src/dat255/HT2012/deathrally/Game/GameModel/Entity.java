eb9df71598721b113f790f6ee4da1dc13e0e3c07
johves <johves@Viper>
2012-10-04 01:15:20 +0200

    Created some classes defined in our architecture. Not much logic though.

---
+++ src/src/dat255/HT2012/deathrally/Game/GameModel/Entity.java
@@ -0,0 +1,9 @@
+package dat255.HT2012.deathrally.Game.GameModel;
+
+import java.util.Observable;
+
+public abstract class Entity extends Observable {
+	
+	public void update(){};
+
+}
---
+++ src/src/dat255/HT2012/deathrally/Game/GameModel/MovableEntity.java
@@ -0,0 +1,5 @@
+package dat255.HT2012.deathrally.Game.GameModel;
+
+public abstract class MovableEntity {
+	public void move(){};
+}
