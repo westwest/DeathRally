9200f46132c26e14d1cfd56e83386ae9dd875a81
Johannes Vestlund <iceman@westlundarna.se>
2012-10-09 10:00:30 +0200

    License
    Change-Id: I2d09e74eb949fc5c3a38ddafdf6c4c853b5f059c
    Signed-off-by: Johannes Vestlund <iceman@westlundarna.se>

--- src/src/dat255/HT2012/deathrally/Game/Visual/VisualEntity.java
+++ src/src/dat255/HT2012/deathrally/Game/Visual/VisualEntity.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2012 Johannes Vestlund <Johannes@westlundarna.se>
+ * 
+ * This file is part of DeathRally
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by 
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even implied warranty of
+ * MERCHANTABILITY or FITTNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package dat255.HT2012.deathrally.Game.Visual;
 
 import javax.microedition.khronos.opengles.GL10;
