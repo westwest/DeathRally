91b6da4ec0fec9a65cf1f0e031a1e5b6b5bf459c
gitta-mannen <mail@janpettersson.com>
2012-10-20 18:23:53 +0200

    Tweaked the MainActivityTest class. Still not working properly. Commit so I can merge branch.
    Signed-off-by: gitta-mannen <mail@janpettersson.com>

--- tests-test/src/dat255/autumn2012/deathrally/test/MainActivityTest.java
+++ tests-test/src/dat255/autumn2012/deathrally/test/MainActivityTest.java
@@ -3,25 +3,29 @@
  */
 package dat255.autumn2012.deathrally.test;
 
-import android.app.Instrumentation;
+import android.app.Activity;
+import android.app.Instrumentation.ActivityMonitor;
+import android.content.Intent;
 import android.test.ActivityInstrumentationTestCase2;
 import android.test.UiThreadTest;
 import android.util.Log;
 import android.widget.Button;
 import dat255.autumn2012.deathrally.*;
 import dat255.autumn2012.deathrally.R;
-import android.util.Log;
+import dat255.autumn2012.deathrally.game.DeathRallyGame;
 
 /**
  * @author Stugatz
- *
+ * Tests the main activity by testing the menu buttons.
  */
 public class MainActivityTest extends ActivityInstrumentationTestCase2 <MainActivity> {
 	private static final String TAG = MainActivityTest.class.getSimpleName();
-	private Instrumentation instrumentation; 
 	private MainActivity mainActivity;
-	private Button startGameButton;
+	private Activity gameActivity;
 	
+	private ActivityMonitor gameActivityMonitor;
+	private Button startGameButton;
+	private Intent i;
 	/**
 	 * @param name
 	 */
@@ -29,42 +33,37 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2 <MainActi
 		super("dat255.autumn2012.deathrally", MainActivity.class);	
 	}
 
-	/* (non-Javadoc)
-	 * @see android.test.ActivityInstrumentationTestCase2#setUp()
-	 */
 	protected void setUp() throws Exception {
 		super.setUp();
-		instrumentation = getInstrumentation();
 		mainActivity = (MainActivity) getActivity();
 		startGameButton = (Button) mainActivity.findViewById(R.id.button1);
-		  Log.d(TAG, startGameButton.getText().toString());
+		
+		// Adds an activity monitor to the game activity class
+		gameActivityMonitor = new ActivityMonitor(DeathRallyGame.class.getName(), null, false);
+		getInstrumentation().addMonitor(gameActivityMonitor);
+		i = new Intent(mainActivity, DeathRallyGame.class).setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 	}
 
-	/* (non-Javadoc)
-	 * @see android.test.ActivityInstrumentationTestCase2#tearDown()
-	 */
-	
+	// Tests the menu button 'new game' to see if it starts the game activity.
 	@UiThreadTest
-	public void test_newGame () {
+	public void testNewGame () {
 		this.setActivityInitialTouchMode(true);
 		
 		mainActivity.runOnUiThread(new Runnable() {
 			  //@Override
 			  public void run() {
 				  startGameButton.performClick();
+				  Log.d(TAG, "Button clicked");
 			  }
 			});
 		
-		//instrumentation.waitForIdleSync();
+		// Check if the game activity was started				
+		//gameActivity = gameActivityMonitor.waitForActivityWithTimeout(2 *1000);
+		//assertNotNull("game activity not started by button", gameActivity);
 		
-		// Test if game window has focus
-		assertEquals(mainActivity.hasWindowFocus(), true);
-		
-	}
-	
-
-	public void testTheTest () {
-		assertEquals(true, true);
+		mainActivity.startActivity(i);
+		gameActivity = gameActivityMonitor.waitForActivityWithTimeout(2 *1000);
+		assertNotNull("game activity not started bu intent", gameActivity);
 	}
 	
 	protected void tearDown() throws Exception {
