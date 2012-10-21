3ea05bebbc3875d35456b2515c2211ec7e497e48
gitta-mannen <mail@janpettersson.com>
2012-10-21 17:17:11 +0200

    More non functioning tweaks..
    Signed-off-by: gitta-mannen <mail@janpettersson.com>

--- tests-test/src/dat255/autumn2012/deathrally/test/MainActivityTest.java
+++ tests-test/src/dat255/autumn2012/deathrally/test/MainActivityTest.java
@@ -20,8 +20,7 @@ import dat255.autumn2012.deathrally.game.DeathRallyGame;
  */
 public class MainActivityTest extends ActivityInstrumentationTestCase2 <MainActivity> {
 	private static final String TAG = MainActivityTest.class.getSimpleName();
-	private MainActivity mainActivity;
-	private Activity gameActivity;
+	private Activity mainActivity, gameActivity;
 	
 	private ActivityMonitor gameActivityMonitor;
 	private Button startGameButton;
@@ -35,8 +34,13 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2 <MainActi
 
 	protected void setUp() throws Exception {
 		super.setUp();
-		mainActivity = (MainActivity) getActivity();
-		startGameButton = (Button) mainActivity.findViewById(R.id.button1);
+		
+		setActivityInitialTouchMode(true);
+		mainActivity = getActivity();
+		startGameButton = (Button) mainActivity.findViewById(R.id.button1);		
+		
+		//check if button has a listener
+		//assertTrue ("button has no listener", startGameButton.getKeyListener() != null);
 		
 		// Adds an activity monitor to the game activity class
 		gameActivityMonitor = new ActivityMonitor(DeathRallyGame.class.getName(), null, false);
@@ -47,23 +51,38 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2 <MainActi
 	// Tests the menu button 'new game' to see if it starts the game activity.
 	@UiThreadTest
 	public void testNewGame () {
-		this.setActivityInitialTouchMode(true);
+
 		
 		mainActivity.runOnUiThread(new Runnable() {
 			  //@Override
 			  public void run() {
+				  startGameButton.requestFocus();
+				  startGameButton.setSelected(true);
+				  boolean listenerHit = startGameButton.performClick();
+				  
+				  // See if we get two monitor hits
 				  startGameButton.performClick();
-				  Log.d(TAG, "Button clicked");
+				  Log.d(TAG, "Button clicked, listener called: " + listenerHit);
 			  }
 			});
 		
 		// Check if the game activity was started				
-		//gameActivity = gameActivityMonitor.waitForActivityWithTimeout(2 *1000);
-		//assertNotNull("game activity not started by button", gameActivity);
-		
-		mainActivity.startActivity(i);
 		gameActivity = gameActivityMonitor.waitForActivityWithTimeout(2 *1000);
-		assertNotNull("game activity not started bu intent", gameActivity);
+		
+		Log.d(TAG, "hits: " + gameActivityMonitor.getHits());
+		Log.d(TAG, "result: " + gameActivityMonitor.getResult());
+		Log.d(TAG, "last activity: " + gameActivityMonitor.getLastActivity());
+		
+		assertNotNull("game activity not started by button", gameActivity);
+
+		//((MainActivity )mainActivity).launchGame(null);		
+		//gameActivity = gameActivityMonitor.waitForActivityWithTimeout(2 *1000);
+		//assertNotNull("game activity not started by launch method", gameActivity);
+		
+		//mainActivity.startActivity(i);
+		//gameActivity = gameActivityMonitor.waitForActivityWithTimeout(2 *1000);
+		//assertNotNull("game activity not started by intent", gameActivity);
+		
 	}
 	
 	protected void tearDown() throws Exception {
